536fc74f77f5b13bdde3070f1a13f1a8aff0f7c8
khelifaYacine <yacine.metal@hotmail.com>
2024-02-29 00:02:09 +0100

    wq

--- Etudes_d_inflation_Mondiale.py
+++ Etudes_d_inflation_Mondiale.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns 
 import matplotlib.pyplot as plt
- from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
-df = pd.read_csv ("C:\Users\yacin\Desktop\ETudes de l'inflation Mondiale\global_inflation_data.csv")
-df.head(10)
-                  
+
+# Lire le fichier CSV
+df = pd.read_csv(r"C:\Users\yacin\Desktop\ETudes de l'inflation Mondiale\global_inflation_data.csv")
+
+# Afficher les dix premières lignes
+df.info()
